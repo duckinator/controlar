adae69c15b66cb8b76d60a4ccb25af685711b5f9
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2014-09-16 11:24:18 -0500

    Coveralls should do this for us

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,5 @@
 require 'coveralls'
 Coveralls.wear!
 
-require 'simplecov'
-
-SimpleCov.configure do
-  add_filter 'spec/'
-end
-SimpleCov.start unless ENV['TRAVIS']
-
-require 'bundler/setup'
-
 require 'rspec'
 
